3acfefdb483b7ed1772b3cb53ef84e29801d10e9
Trailwalker <936519487@qq.com>
2020-02-13 21:11:09 +0800

    增加了新闻页的功能

--- app/src/main/java/com/trailwalker/myapplication/MainActivity.java
+++ app/src/main/java/com/trailwalker/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.trailwalker.myapplication;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"新闻来了，反对996工作制", Toast.LENGTH_SHORT).show();
     }
 }
